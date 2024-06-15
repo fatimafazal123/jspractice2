884dffeffa32dc5e66fc22e4887f00335b48f244
Fatima Fazal <159261117+fatimafazal123@users.noreply.github.com>
2024-06-15 20:02:28 +0500

    js practice 2

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// var first = 100;
+// first *= 2;
+// first -= 20;
+// first +=300;
+// first /= 5;
+// console.log(first);
+
+// var second = prompt ("Enter Your City Name Here");
+
+// var prompt ("Enter Your Word")
+// var word = "radar"
+// for (var i = word.length-1 ; i >= 0 ; i--) {
+//     console.log(word[i]);
+}
+// var word = "mom";
+// for(var i = word.length-1 ; i )
+// if(palindrom=palindrom)
